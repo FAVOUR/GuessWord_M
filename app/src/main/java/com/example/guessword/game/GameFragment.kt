a225968ccf3ac7831434d036cc6d3412027d8332
FAVOUR <truelyfavoured@gmail.com>
2020-07-30 11:36:44 +0100

    [Add viewModels]-Done with implementing viemodel for GameFragment.kt  and navigation

--- app/src/main/java/com/example/guessword/game/GameFragment.kt
+++ app/src/main/java/com/example/guessword/game/GameFragment.kt
@@ -6,26 +6,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.NavHostFragment
 import com.example.guessword.R
 import com.example.guessword.databinding.FragmentGameBinding
 
 class GameFragment : Fragment() {
 
-    // The current word
-    private var word = ""
-
-    // The current score
-    private var score = 0
-
-
-    // The list of words - the front of the list is the next word to guess
-    private lateinit var wordList: MutableList<String>
 
     private lateinit var binding: FragmentGameBinding
-    private lateinit var viewmodel: GameViewModel
+    private lateinit var viewModel: GameViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,12 +28,15 @@ class GameFragment : Fragment() {
         binding = DataBindingUtil.inflate(inflater,R.layout.fragment_game, container, false)
 
         Log.i("GameFragment", "Called ViewModelProviders.of")
-        viewmodel = ViewModelProviders.of(this)[GameViewModel::class.java]
-        resetList()
-        nextWord()
+        viewModel = ViewModelProviders.of(this)[GameViewModel::class.java]
+
+
 
         binding.correctButton.setOnClickListener { onCorrect() }
         binding.skipButton.setOnClickListener { onSkip() }
+
+        binding.endGameButton.setOnClickListener { onEndGame() }
+
         updateScoreText()
         updateWordText()
         return binding.root
@@ -48,81 +44,46 @@ class GameFragment : Fragment() {
 
     }
 
-    /**
-     * Resets the list of words and randomizes the order
-     */
-    private fun resetList() {
-        wordList = mutableListOf(
-            "queen",
-            "hospital",
-            "basketball",
-            "cat",
-            "change",
-            "snail",
-            "soup",
-            "calendar",
-            "sad",
-            "desk",
-            "guitar",
-            "home",
-            "railway",
-            "zebra",
-            "jelly",
-            "car",
-            "crow",
-            "trade",
-            "bag",
-            "roll",
-            "bubble"
-        )
-        wordList.shuffle()
-    }
-
-
 
     /** Methods for buttons presses **/
 
     private fun onSkip() {
-        if (!wordList.isEmpty()) {
-            score--
-        }
-        nextWord()
-    }
-
-
-    private fun onCorrect() {
-        if (!wordList.isEmpty()) {
-            score++
-        }
-        nextWord()
-    }
-
-
-
-    /**
-     * Moves to the next word in the list
-     */
-    private fun nextWord() {
-        if (!wordList.isEmpty()) {
-            //Select and remove a word from the list
-            word = wordList.removeAt(0)
-        }
+        viewModel.onSkip()
         updateWordText()
         updateScoreText()
     }
+    private fun onCorrect() {
+        viewModel.onCorrect()
+        updateScoreText()
+        updateWordText()
 
+
+    }
 
 
     /** Methods for updating the UI **/
 
     private fun updateWordText() {
-        binding.wordText.text = word
+        binding.wordText.text =viewModel.word
     }
 
     private fun updateScoreText() {
-        binding.scoreText.text = score.toString()
+        binding.scoreText.text = viewModel.score.toString()
+    }
+
+    /**
+     * Called when the game is finished
+     */
+    private fun gameFinished() {
+        Toast.makeText(activity, "Game has just finished", Toast.LENGTH_SHORT).show()
+        val action = GameFragmentDirections.actionGameFragmentToScoreFragment()
+        action.score = viewModel.score
+        NavHostFragment.findNavController(this).navigate(action)
     }
 
 
+    private fun onEndGame() {
+        gameFinished()
+    }
 
 }
